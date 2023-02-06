40f20e48a02b7cb7cc51432c25b802642c0ab176
Mohammad Olid Afzal <123568599+olid2011831042@users.noreply.github.com>
2023-02-06 10:38:49 +0600

    Add files via upload

---
+++ Desimal_Octal_Hexadesimal_Number_Conversion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int number ,num1,num2;
+
+    //decimal to octal
+printf("Decimal to Octal conversion\n");
+printf("Enter any Decimal number= ");
+scanf("%d",&number);
+printf("Octal number is= %o\n",number);
+
+//Decimal to hexadecimal
+printf("Decimal to Hexadecimal conversion\n");
+printf("Enter any Decimal number= ");
+scanf("%d",&num1);
+printf("HexaDecimal number is= %x\n",number);
+
+//Octal to hexadecimal
+printf("Octal to Hexadecimal conversion\n");
+printf("Enter any Decimal number= ");
+scanf("%0",&num2);
+printf("Hexadecimal number is= %x",number);
+
+
+}
